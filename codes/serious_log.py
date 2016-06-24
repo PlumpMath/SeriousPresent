61f7edb4ab478aa6b2f9b18439c828bf8d123708
CodingBlack <451718145@qq.com>
2016-06-24 19:49:15 +0800

    部分修改 serious_log。

--- codes/serious_log.py
+++ codes/serious_log.py
@@ -10,6 +10,15 @@ import logging
 import logging.handlers
 
 class SeriousLog(object):
+
+    @property
+    def file_name(self):
+        return self.__file_name
+
+    @file_name.setter
+    def file_name(self,file_name):
+        self.__file_name = file_name
+
     # 初始化函数
     # IN: 希望日志记录的文件的文件名称
     def __init__(self,file_name):
@@ -18,9 +27,9 @@ class SeriousLog(object):
     # 日志函数
     # IN：level--显示的错误等级（分为info,warn,error三个等级,使用字符串即可） content--希望显示的日志信息内容（字符串）
     def log(self,level,content):
-        LOG_FILE = self.file_name
+        log_file = self.__file_name
 
-        handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024 * 1024, backupCount=5)
+        handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=5)
         fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
 
         formatter = logging.Formatter(fmt)  # 实例化formatter
