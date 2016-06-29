5aacc580ca439aa674cae048145ea2f78dc815f9
CodingBlack <451718145@qq.com>
2016-06-29 15:22:01 +0800

    修改 ControlModule 目录的文件结构。 整理组织结构。

--- codes/ControlModule/input_handler.py
+++ codes/ControlModule/input_handler.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from direct.showbase.DirectObject import DirectObject
 from direct.showbase.MessengerGlobal import messenger
 from direct.task.TaskManagerGlobal import taskMgr
@@ -7,12 +8,60 @@ class InputHandler(DirectObject):
     def __init__(self):
         DirectObject.__init__(self)
 
+        # 所有的状态类别
+        self.menu = False
+        self.load = False
+        self.game = False
+        self.save = False
+        self.exitMenu = False
+
+        # 人物动作
         self.walk = False
         self.reverse = False
         self.left = False
         self.right = False
 
         taskMgr.add(self.updateInput,"update input")
+
+    def beginMenu():
+        messenger.send("Menu-start")
+        self.menu = True
+
+    def endMenu():
+        messenger.send("Menu-stop")
+        self.menu = False
+
+    def beginload():
+        messenger.send("load-start")
+        self.load = True
+
+    def endload():
+        messenger.send("load-stop")
+        self.load = False
+
+    def begingame():
+        messenger.send("game-start")
+        self.game = True
+
+    def endgame():
+        messenger.send("game-stop")
+        self.game = False
+
+    def beginsave():
+        messenger.send("save-start")
+        self.save = True
+
+    def endsave():
+        messenger.send("save-stop")
+        self.save = False
+
+    def beginexitMenu():
+        messenger.send("exitMenu-start")
+        self.exitMenu = True
+
+    def endexitMenu():
+        messenger.send("exitMenu-stop")
+        self.exitMenu = False
 
     def beginWalk(self):
         messenger.send("walk-start")
