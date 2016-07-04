ca422011c2a166bff899fb2f75c1285928a54787
huiresurgeran <313829429@qq.com>
2016-07-04 16:26:06 +0830

    加入角色类存档

--- codes/ResourcesModule/archives.py
+++ codes/ResourcesModule/archives.py
@@ -43,44 +43,59 @@ class Archives(object):
 
         return self.__showArchives
 
-    def click_archive(self,id):
-        if id==0 :
-            pass
-        else :
-            for i in range(len(self.__archives)):
-                if (self.__archives[i]["id"] == id):
-                    self.__loadSceneArchive = self.__archives[i]["content"]["scene"]
-                    # self.__loadRoleArchive = self.__archives[i]["content"]["role"]
-
     #存档
-    def save_archive(self,sceneArchive,id,archiveName):
-        if id==0 :
+    def save_archive(self,sceneArchive,roleArchive,id,archiveName):
+        #新存档
+        if id==-1 :
             id = len(self.__archives)+1
-            self.new_archive(sceneArchive,id,archiveName)
+            self.new_archive(sceneArchive,roleArchive,id,archiveName)
             self.__archives.append(self.__archive)
+        #覆盖之前的存档
         else:
             for i in range(len(self.__archives)):
                 if (self.__archives[i]["id"] == id):
-                    self.new_archive(sceneArchive,id,archiveName)
+                    self.new_archive(sceneArchive,roleArchive,id,archiveName)
                     self.__archives[i]=self.__archive
 
         self.write_to_file()
 
     #新建存档
-    def new_archive(self,sceneArchive,id,archiveName):
+    def new_archive(self,sceneArchive,roleArchive,id,archiveName):
         #场景类存档
         self.__sceneArchive=dict()
+        self.__roleArchive = dict()
+
+        self.new(sceneArchive,self.__sceneArchive)
+        self.new(roleArchive,self.__roleArchive)
+
+        # 一条存档
+        self.__archive = dict()
+        self.__archive["id"] = id
+        self.__archive["time"] = time.strftime('%Y/%m/%d %H:%M', time.localtime(time.time()))
+        self.__archive["name"] = archiveName
+        self.__archive["content"] = dict()
+        self.__archive["content"]["scene"] = self.__sceneArchive
+        self.__archive["content"]["role"] = self.__roleArchive
+
+        # self.select_archive(1)
+
+        # print self.__archive["content"]["scene"]["camera"]
+
+        self.write_to_file()
+
+    #场景与角色存档
+    def new(self,archive,archived):
 
         index = 0
         indexChar=0
-        for lists in sceneArchive:
-            name=lists.get_metaData()["ArchivePackageName"]
-            self.__sceneArchive[name] = dict()
-            self.__sceneArchive[name]["metaData"] = lists.get_metaData()
-            self.__sceneArchive[name]["items"]= list()
+        for lists in archive:
+            name=lists.get_all_metaData()["ArchivePackageName"]
+            archived[name] = dict()
+            archived[name]["metaData"] = lists.get_all_metaData()
+            archived[name]["items"]= list()
 
             for i in range(len(lists.get_itemsData())):
-                self.__sceneArchive[name]["items"].append(dict())
+                archived[name]["items"].append(dict())
                 for j in range(len(lists.get_itemsName())):
                     #去掉LPoint3f(0, 50, 5)前缀
                     indexChar = str((lists.get_itemsData())[i][j]).find("(")
@@ -93,66 +108,64 @@ class Archives(object):
                         for item in data.split(','):
                             itemData.append(float(item))
 
-                    self.__sceneArchive[name]["items"][i][(lists.get_itemsName())[j]] = itemData
+                    archived[name]["items"][i][(lists.get_itemsName())[j]] = itemData
 
             index = index+1
-
-        #一条存档
-        self.__archive=dict()
-        self.__archive["id"] = id
-        self.__archive["time"]=time.strftime('%Y/%m/%d %H:%M',time.localtime(time.time()))
-        self.__archive["name"]=archiveName
-        self.__archive["content"]=dict()
-        self.__archive["content"]["scene"]=self.__sceneArchive
-        # self.__archive["content"]["role"] = self.__role
-
-        # self.select_archive(1)
-
-        # print self.__archive["content"]["scene"]["camera"]
 
     #选择存档
     def select_archive(self,id):
         for i in range(len(self.__archives)):
             if(self.__archives[i]["id"]==id):
                 self.__loadSceneArchive=self.__archives[i]["content"]["scene"]
-                # self.__loadRoleArchive = self.__archives[i]["content"]["role"]
+                self.__loadRoleArchive = self.__archives[i]["content"]["role"]
         self.read_archive()
+
+        return [self.__selectedSceneArchive,self.__selectedRoleArchive]
 
     #读取档案，加载游戏
     def read_archive(self):
+        #场景类加载
         self.__selectedSceneArchive=list()
-        for name in self.__loadSceneArchive:
-            arc=ArchivePackage(name,self.__loadSceneArchive[name]["items"][0].keys())
-            for key in self.__loadSceneArchive[name]["metaData"]:
-                arc.append_metaData(key,self.__loadSceneArchive[name]["metaData"][key])
-            arc.__metaData=self.__loadSceneArchive[name]["metaData"]
-            for index in range(len(self.__loadSceneArchive[name]["items"])):
+        # 角色类加载
+        self.__selectedRoleArchive = list()
+
+        self.read(self.__loadSceneArchive,self.__selectedSceneArchive)
+        self.read(self.__loadRoleArchive, self.__selectedRoleArchive)
+
+        # print self.__selectedRoleArchive[0].get_all_metaData()
+        # print self.__selectedRoleArchive[0].get_itemsName()
+        # print self.__selectedRoleArchive[0].get_itemsData()[0]
+
+    #读取场景类与角色类
+    def read(self,loadArchive,selectedArchive):
+        for name in loadArchive:
+            arc=ArchivePackage(name,loadArchive[name]["items"][0].keys())
+            for key in loadArchive[name]["metaData"]:
+                arc.append_metaData(key,loadArchive[name]["metaData"][key])
+            arc.__metaData=loadArchive[name]["metaData"]
+            for index in range(len(loadArchive[name]["items"])):
                 addItem=list()
-                for key ,item in self.__loadSceneArchive[name]["items"][index].items():
+                for key ,item in loadArchive[name]["items"][index].items():
                     if key == "pos":
-                        item= LPoint3f(self.__loadSceneArchive[name]["items"][index][key][0],
-                                       self.__loadSceneArchive[name]["items"][index][key][1],
-                                       self.__loadSceneArchive[name]["items"][index][key][2])
+                        item= LPoint3f(loadArchive[name]["items"][index][key][0],
+                                       loadArchive[name]["items"][index][key][1],
+                                       loadArchive[name]["items"][index][key][2])
                     elif key == "hpr":
-                        item = LVecBase3f(self.__loadSceneArchive[name]["items"][index][key][0],
-                                          self.__loadSceneArchive[name]["items"][index][key][1],
-                                          self.__loadSceneArchive[name]["items"][index][key][2])
+                        item = LVecBase3f(loadArchive[name]["items"][index][key][0],
+                                          loadArchive[name]["items"][index][key][1],
+                                          loadArchive[name]["items"][index][key][2])
                     elif key == "scale":
-                        item = LVecBase3f(self.__loadSceneArchive[name]["items"][index][key][0],
-                                          self.__loadSceneArchive[name]["items"][index][key][1],
-                                          self.__loadSceneArchive[name]["items"][index][key][2])
+                        item = LVecBase3f(loadArchive[name]["items"][index][key][0],
+                                          loadArchive[name]["items"][index][key][1],
+                                          loadArchive[name]["items"][index][key][2])
                     elif key == "color":
-                        item = LVecBase4f(self.__loadSceneArchive[name]["items"][index][key][0],
-                                          self.__loadSceneArchive[name]["items"][index][key][1],
-                                          self.__loadSceneArchive[name]["items"][index][key][2],
-                                          self.__loadSceneArchive[name]["items"][index][key][3])
+                        item = LVecBase4f(loadArchive[name]["items"][index][key][0],
+                                          loadArchive[name]["items"][index][key][1],
+                                          loadArchive[name]["items"][index][key][2],
+                                          loadArchive[name]["items"][index][key][3])
                     addItem.append(item)
                     arc.add_item(addItem)
-            self.__selectedSceneArchive.append(arc)
-
-        # print self.__selectedSceneArchive[2].get_metaData()
-        # print self.__selectedSceneArchive[2].get_itemsName()
-        # print self.__selectedSceneArchive[2].get_itemsData()
+            selectedArchive.append(arc)
 
     #将存档记录写入文件
     def write_to_file(self):
