641e94a11d99abf9055cba83d7a516dbd54fffbc
huiresurgeran <313829429@qq.com>
2016-07-05 14:38:49 +0830

    更改存档文件

--- codes/ResourcesModule/archives.py
+++ codes/ResourcesModule/archives.py
@@ -123,10 +123,18 @@ class Archives(object):
 
     #选择存档
     def select_archive(self,id):
-        for i in range(len(self.__archives)):
-            if(self.__archives[i]["id"]==id):
-                self.__loadSceneArchive=self.__archives[i]["content"]["scene"]
-                self.__loadRoleArchive = self.__archives[i]["content"]["role"]
+
+        if id == 0 :#开始新的游戏，读取初始档
+            self.init_archive()
+            self.__loadSceneArchive = self.__initArchives["content"]["scene"]
+            self.__loadRoleArchive = self.__initArchives["content"]["role"]
+
+        else:
+            for i in range(len(self.__archives)):
+                if(self.__archives[i]["id"]==id):
+                    self.__loadSceneArchive=self.__archives[i]["content"]["scene"]
+                    self.__loadRoleArchive = self.__archives[i]["content"]["role"]
+
         self.read_archive()
 
         return [self.__selectedSceneArchive,self.__selectedRoleArchive]
@@ -141,42 +149,62 @@ class Archives(object):
         self.read(self.__loadSceneArchive,self.__selectedSceneArchive)
         self.read(self.__loadRoleArchive, self.__selectedRoleArchive)
 
-        # print self.__selectedSceneArchive[1].get_all_metaData()
-        # print self.__selectedSceneArchive[1].get_itemsName()
-        # print self.__selectedSceneArchive[1].get_itemsData()[0]
+        print self.__selectedSceneArchive[2].get_all_metaData()
+        print self.__selectedSceneArchive[2].get_itemsName()
+        print self.__selectedSceneArchive[2].get_itemsData()[0]
+
+    def byteify(self,input):
+        if isinstance(input, dict):
+            return {self.byteify(key): self.byteify(value) for key, value in input.iteritems()}
+        elif isinstance(input, list):
+            return [self.byteify(element) for element in input]
+        elif isinstance(input, unicode):
+            return input.encode('utf-8')
+        else:
+            return input
 
     #读取场景类与角色类
     def read(self,loadArchive,selectedArchive):
+        loadArchive = self.byteify(loadArchive)
+
         for name in loadArchive:
             keys=list()
+
             for l in range(len(loadArchive[name]["items"][0])):
-                keys.append(''.join(loadArchive[name]["items"][0][l].keys()))
+                for j in loadArchive[name]["items"][0][l].keys():
+                    keys.append(j)
+
             arc=ArchivePackage(name,keys)
+
+            #metaData
             for key in loadArchive[name]["metaData"]:
-                arc.append_metaData(key,loadArchive[name]["metaData"][key])
-            arc.__metaData=loadArchive[name]["metaData"]
+                arc.append_metaData(key, loadArchive[name]["metaData"][key])
+
+            #itemData
             for index in range(len(loadArchive[name]["items"])):
                 addItem=list()
                 for l in range(len(loadArchive[name]["items"][index])):
 
                     for key ,item in loadArchive[name]["items"][index][l].items():
-                        if key == "pos":
+
+                        if key == "pos" or key == "Pos":
                             item= LPoint3f(loadArchive[name]["items"][index][l][key][0],
                                            loadArchive[name]["items"][index][l][key][1],
                                            loadArchive[name]["items"][index][l][key][2])
-                        elif key == "hpr":
+                        elif key == "hpr" or key == "Hpr":
                             item = LVecBase3f(loadArchive[name]["items"][index][l][key][0],
                                               loadArchive[name]["items"][index][l][key][1],
                                               loadArchive[name]["items"][index][l][key][2])
-                        elif key == "scale":
+                        elif key == "scale" or key == "Scale":
                             item = LVecBase3f(loadArchive[name]["items"][index][l][key][0],
                                               loadArchive[name]["items"][index][l][key][1],
                                               loadArchive[name]["items"][index][l][key][2])
-                        elif key == "color":
+                        elif key == "color" or key == "Color":
                             item = LVecBase4f(loadArchive[name]["items"][index][l][key][0],
                                               loadArchive[name]["items"][index][l][key][1],
                                               loadArchive[name]["items"][index][l][key][2],
                                               loadArchive[name]["items"][index][l][key][3])
+
                         addItem.append(item)
                         arc.add_item(addItem)
 
