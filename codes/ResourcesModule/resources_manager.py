21b0225024c7f2ee5a59df0d56269ae33c1e354f
huiresurgeran <313829429@qq.com>
2016-07-07 16:10:53 +0830

    增加暂停设置界面

--- codes/ResourcesModule/resources_manager.py
+++ codes/ResourcesModule/resources_manager.py
@@ -39,18 +39,18 @@ class ResourcesManager(object):
     def stop_sound(self, id):
         self.__sound.stop_music(id)
 
-    #开关背景音乐
-    def play_background_music(self):
-
-        self.__sound.toggleMusicBox()
-
-    #加载声音控制滑动条控件
-    def show_volume_sliderbar(self):
-        self.__sound.volume_slider()
+    #初始化暂停设置界面控件
+    def show_volume_sliderbar(self,base):
+        self.__sound.init_setting(base)
 
     #移除滑动条等有关声音的控件
     def destroy_volume_sliderbar(self):
         self.__sound.destroy()
+
+    # #开关背景音乐
+    # def play_background_music(self):
+    #
+    #     self.__sound.toggleMusicBox()
 
     """""""""""
     视频函数
--- codes/ResourcesModule/sound.py
+++ codes/ResourcesModule/sound.py
@@ -13,7 +13,9 @@ from direct.gui.OnscreenImage import OnscreenImage
 from direct.showbase.DirectObject import DirectObject
 from direct.gui.DirectSlider import DirectSlider
 from direct.gui.DirectButton import DirectButton
+from pandac.PandaModules import TransparencyAttrib
 from direct.interval.SoundInterval import SoundInterval
+from panda3d.core import *
 
 
 class MySound(DirectObject):
@@ -21,74 +23,163 @@ class MySound(DirectObject):
     def __init__(self):
 
         self.__music=dict()
-        self.__music["1"]=loader.loadMusic("../../resources/music/test.mp3")#背景音乐
-        self.__music["2"] = loader.loadSfx('../../resources/music/openclose.ogg')#平时音效
-        self.__music["3"] = loader.loadSfx('../../resources/music/openclose.ogg')  # 战斗音效
-        self.__music["4"] = loader.loadSfx('../../resources/music/openclose.ogg')#对话音效
-        self.__music["5"] = loader.loadSfx('../../resources/music/openclose.ogg')#枪击音效
-        self.__music["6"] = loader.loadSfx('../../resources/music/openclose.ogg')#载入游戏音效
-        self.__music["7"] = loader.loadSfx('../../resources/music/openclose.ogg')#退出游戏音效
+        self.__music["1"]=loader.loadMusic("../../resources/music/test.mp3")#主界面背景音乐
+        self.__music["2"] = loader.loadMusic('../../resources/music/test.mp3')#平时音乐
+        self.__music["3"] = loader.loadMusic('../../resources/music/test.mp3')#战斗音乐
+        # self.__music["4"] = loader.loadSfx('../../resources/music/openclose.ogg')#对话音效
+        self.__music["4"] = loader.loadSfx('../../resources/music/openclose.ogg')#枪击音效
+        self.__music["5"] = loader.loadSfx('../../resources/music/openclose.ogg')#载入游戏音效
+        self.__music["6"] = loader.loadSfx('../../resources/music/openclose.ogg')#退出游戏音效
 
-        self.__music["1"].setVolume(0.5)
-        self.__music["1"].setLoop(True)
+        #设置音乐与音效的音量
+        for index in self.__music:
+            self.__music[index].setVolume(0.5)
+
+        # 设置音乐与音效的循环次数
+        self.__music["1"].setLoop(0)
+        self.__music["2"].setLoop(0)
+        self.__music["3"].setLoop(0)
+        self.__music["4"].setLoop(1)
+        self.__music["5"].setLoop(1)
+        self.__music["6"].setLoop(1)
 
         self.__volume=0.5#滑动条值
 
         self.__musicTime = 0#背景音乐所处时间
 
-        self.__backgroundId=1
+        self.__backgroundId=1#背景音乐ID
 
-        self.__musicOpen = True
+        self.__musicOpen = True#音乐是否开启
 
-        self.__music["1"].play()
+        self.__music["1"].play()#主界面背景音乐自动开启
+
+        self.__destroy=False#是否可以移除控件
 
 
-    #初始化声音滑动条，开关声音按钮
-    def volume_slider(self):
-        self.__sliderText = OnscreenText("Volume", pos=(0, 0.87), scale=0.07, fg=(1, 1, 1, 1), shadow=(0, 0, 0, 1))
-        self.__slider = DirectSlider(pos=(-0.1, 0, 0.75), scale=0.8, value=0.5, command=self.setMusicBoxVolume)
-        self.__musicButton = DirectButton(pos=(0.9, 0, 0.75), text="Close", scale=0.1, pad=(0.2, 0.2), rolloverSound=None,
-                                     clickSound=None, command=self.toggleMusicBox)
+    #初始化暂停设置界面
+    def init_setting(self,base):
+        if self.__destroy==False:
+            self.__image = OnscreenImage(image='../../resources/images/menu/home1.png', pos=(0, 0, 0), scale=1)
+            self.__image.setSx(base.getAspectRatio())
+            self.__image.setTransparency(TransparencyAttrib.MAlpha)
 
-        self.__slider['value']=self.__volume
+            #设置界面背景图
+            self.__background = OnscreenImage(image='../../resources/images/settings/setting_frame.png', pos=(0, 0, 0), scale=(1.0,0,0.7))
+            self.__background.setTransparency(TransparencyAttrib.MAlpha)
+
+            ##滑动条
+            self.__slider = DirectSlider(pos=(0.16, 0, 0.26), scale=0.5, value=0.5, command=self.setMusicBoxVolume,
+                                         frameSize=(-1.0, 0.9, -0.06, 0.06) , image = '../../resources/images/settings/slide_bar.png',
+                                         image_pos = (-0.05, 0, 0.0), image_scale = (1.0,0,0.05),
+                                         thumb_image='../../resources/images/settings/slide_btn.png',
+                                         thumb_image_pos=(-0.0, 0, 0.0),thumb_image_scale=0.1,thumb_frameSize=(0.0, 0.0,0.0, 0.0))
+            self.__slider.setTransparency(TransparencyAttrib.MAlpha)
+
+
+            # self.__musicButton = DirectButton(pos=(0.9, 0, 0.75), text="Close", scale=0.1, pad=(0.2, 0.2), rolloverSound=None,
+            #                                   clickSound=None, command=self.toggleMusicBox,extraArgs=[base])
+
+            #继续按钮
+            self.__continueButton=DirectButton(pos=(-0.25, 0, 0.0), text="", scale=(0.2,0,0.1),command=self.continue_game,
+                                               image=("../../resources/images/settings/btn_continue_0.png",
+                                                      "../../resources/images/settings/btn_continue_0.png"
+                                                      ,"../../resources/images/settings/btn_continue_1.png"),
+                                               frameColor=(0,0,0,0))
+            self.__continueButton.setTransparency(TransparencyAttrib.MAlpha)
+
+            #存档按钮
+            self.__saveButton = DirectButton(pos=(0.33, 0, 0.0), text="", scale=(0.2, 0, 0.1),command=self.save_game,
+                                                 image=("../../resources/images/settings/btn_save_0.png",
+                                                        "../../resources/images/settings/btn_save_0.png"
+                                                        , "../../resources/images/settings/btn_save_1.png"),
+                                                 frameColor=(0, 0, 0, 0))
+            self.__saveButton.setTransparency(TransparencyAttrib.MAlpha)
+
+            #帮助按钮
+            self.__helpButton = DirectButton(pos=(-0.25, 0, -0.25), text="", scale=(0.2, 0, 0.1),command=self.help,
+                                                 image=("../../resources/images/settings/btn_help_0.png",
+                                                        "../../resources/images/settings/btn_help_0.png"
+                                                        , "../../resources/images/settings/btn_help_1.png"),
+                                                 frameColor=(0, 0, 0, 0))
+            self.__helpButton.setTransparency(TransparencyAttrib.MAlpha)
+
+            #回到主界面按钮
+            self.__homeButton = DirectButton(pos=(0.33, 0, -0.25), text="", scale=(0.2, 0, 0.1),command=self.return_home,
+                                                 image=("../../resources/images/settings/btn_home_0.png",
+                                                        "../../resources/images/settings/btn_home_0.png"
+                                                        , "../../resources/images/settings/btn_home_1.png"),
+                                                 frameColor=(0, 0, 0, 0))
+            self.__homeButton.setTransparency(TransparencyAttrib.MAlpha)
+
+            #设置滑动条value
+            self.__slider['value']=self.__volume
+
+            self.__destroy=True
 
     #移除所有控件
     def destroy(self):
-        self.__volume=self.__slider['value']
-        self.__slider.destroy()
-        self.__sliderText.destroy()
-        self.__musicButton.destroy()
+        if self.__destroy==True:
+            self.__background.destroy()
+            self.__volume=self.__slider['value']
+            self.__slider.destroy()
+            # self.__musicButton.destroy()
+            self.__continueButton.destroy()
+            self.__saveButton.destroy()
+            self.__helpButton.destroy()
+            self.__homeButton.destroy()
+            self.__destroy=False
 
-    #设置背景音乐声音大小
+    #设置音乐声音大小
     def setMusicBoxVolume(self):
+        print self.__slider.guiItem
         newVolume=self.__slider.guiItem.getValue()
-        self.__music["1"].setVolume(newVolume)
+        for index in self.__music:
+            self.__music[index].setVolume(newVolume)
 
     #开关背景音乐
-    def toggleMusicBox(self):
+    def toggleMusicBox(self,base):
 
         #关闭音乐
         if (self.__musicOpen==True):
-            for index in self.__music:
-                index.stop()
-            self.__musicButton["text"]="Open"
+            base.disableAllAudio()
+            # self.__musicButton["text"]="Open"
         #开启音乐
         else:
-            self.__music[self.__backgroundId].play()
-            self.__musicButton["text"]="Close"
+            base.enableAllAudio()
+            self.__music[str(self.__backgroundId)].play()
+            # self.__musicButton["text"]="Close"
 
-        self.__musicButton.setText()
+        # self.__musicButton.setText()
         self.__musicOpen=not self.__musicOpen
 
+    #播放音乐
+    #id：音乐与音效id
     def play_music(self,id):
         if(id==1 or id==2 or id==3):
             self.__backgroundId=id
         id=str(id)
         self.__music[id].play()
 
+    #关闭音乐
     def stop_music(self,id):
         id=str(id)
         self.__music[id].stop()
+
+    #继续游戏
+    def continue_game(self):
+        self.destroy()
+
+    #存档
+    def save_game(self):
+        self.destroy()
+
+    #游戏帮助
+    def help(self):
+        self.destroy()
+
+    #回到主界面
+    def return_home(self):
+        self.destroy()
 
     # #播放战斗音效
     # def play_battle_music(self):
